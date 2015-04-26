d3f77b19c27364dc4cce13ea3843c62e9bd41e8a
urla <urla48@yandex.ru>
2015-04-26 22:13:30 +0300

    commite js

--- app/js/main.js
+++ app/js/main.js
@@ -1,125 +1,160 @@
-$(function () {
-    var Popup = function(selector) {
+var Popup = function (selector) {
+    var obj = {
+        $el: $(selector),
+        init: function () {
+            var t = this;
+            this.$el.find('.close, .pop-up__overlay').on('click', function () {
+                t.hide();
+            });
+        },
 
-        var obj = {
-            $el: $(selector),
-            init: function () {
-                var t = this;
-                this.$el.find('.close, .pop-up__overlay').on('click', function () {
-                    t.hide();
-                });
-            },
-            show: function () {
-                this.$el.css({'display': 'block'});
-            },
-            hide: function () {
-                this.$el.css({'display': 'none'});
-            }
-        };
+        show: function () {
+            this.$el.css({'display': 'block'});
+        },
 
-        obj.init();
-        return obj;
+        hide: function () {
+            this.$el.css({'display': 'none'});
+        }
     };
 
-    var addProjectPopup = new Popup('.pop-up_add');
+    obj.init();
+    return obj;
+};
 
+
+var validate = {
+    /**
+     * Проверка всех инпутов формы
+     * @param $form
+     * @returns {boolean}
+     */
+    checkData: function ($form) {
+        this.$form = $form;
+        var t = this;
+        this.valid = true;
+
+        this.$form.find('.form__input').each(function () {
+            t.checkInput($(this));
+        });
+
+        return this.valid;
+    },
+
+    /**
+     * Проверка одного инпута
+     * @param $input
+     */
+    checkInput: function ($input) {
+        if ($input.val() == '') {
+            this.showTooltip($input);
+            this.addError($input);
+            this.valid = false;
+        } else {
+            this.clearOne($input);
+        }
+    },
+
+    /**
+     * Показать тултип
+     * @param $input
+     */
+    showTooltip: function ($input) {
+        $input.closest('.form__group').find('.tooltip').css({'display': 'inline-block'});
+    },
+
+    /**
+     * Показать ошибку в инпуте
+     * @param $input
+     */
+    addError: function ($input) {
+        $input.addClass('error');
+    },
+
+    /**
+     * Очистить все поля от ошибок
+     * @param $form
+     */
+    clearAll: function ($form) {
+        this.$form = $form;
+        var t = this;
+
+        this.$form.find('.form__input').each(function () {
+            t.clearOne($(this))
+        });
+    },
+
+    /**
+     * Очистить одно поле от ошибок
+     * @param $input
+     */
+    clearOne: function ($input) {
+        $input.closest('.form__group').find('.tooltip').css({'display': 'none'});
+        $input.removeClass('error');
+    }
+};
+
+$(function () {
+
+    //Попап добавления проекта
+    var addProjectPopup = new Popup('.pop-up_add');
     $('.add').on('click', function () {
         addProjectPopup.show();
     });
 
+    //Попап просмотра проектов
     var galleryPopup = new Popup('.pop-up_gallery');
-
     $('.portfolio__img-hover').on('click', function () {
         galleryPopup.$el.find('.gallery__img').attr('src', $(this).data('img'));
         galleryPopup.show();
     });
+
+    //Попап успеха)
     var successPopup = new Popup('.pop-up_success');
 
-    var validate = {
-        checkData: function ($form) {
-            this.$form = $form;
-            var t = this;
-            this.valid = true;
 
-            this.$form.find('.form__input').each(function(){
-                t.checkInput($(this));
-            });
-
-            return this.valid;
-        },
-
-        checkInput: function ($input) {
-            if($input.val() == ''){
-                this.showTooltip($input);
-                this.addError($input);
-                this.valid = false;
-            } else {
-                this.clearOne($input);
-            }
-        },
-
-        showTooltip: function ($input) {
-            $input.closest('.form__group').find('.tooltip').css({'display': 'inline-block'});
-        },
-        addError: function ($input) {
-            $input.addClass('error');
-        },
-
-        clearAll: function ($form) {
-            this.$form = $form;
-            var t = this;
-
-            this.$form.find('.form__input').each(function(){
-                t.clearOne($(this))
-            });
-        },
-
-        clearOne: function ($input) {
-            $input.closest('.form__group').find('.tooltip').css({'display': 'none'});
-            $input.removeClass('error');
-        }
-    };
-
+    //Форма авторизации
     var $formLogin = $('#login-form');
-
     $formLogin.on('submit', function (e) {
         e.preventDefault();
-        if(validate.checkData($(this))) {
+        if (validate.checkData($(this))) {
+            //todo luneva сделать авторизацию
         }
     });
-    var $formAddProject = $('#add-project-form');
 
+    //Форма добавления проекта
+    var $formAddProject = $('#add-project-form');
     $formAddProject.on('submit', function (e) {
         e.preventDefault();
-        if(validate.checkData($(this))) {
+        if (validate.checkData($(this))) {
             addProjectPopup.hide();
             successPopup.show();
         }
     });
 
+    //Форма обратной связи
     var $formSend = $('#massage-form');
-
     $formSend.on('submit', function (e) {
         e.preventDefault();
-        if(validate.checkData($(this))) {
+        if (validate.checkData($(this))) {
             var form_data = $(this).serialize(); //собераем все данные из формы
             $.ajax({
                 type: "POST", //Метод отправки
                 url: "php/send.php", //путь до php фаила отправителя
                 data: form_data,
-                success: function() {
-                    //код в этом блоке выполняется при успешной отправке сообщения
+                success: function () {
                     alert("Ваше сообщение отпрвлено!");
-                    location.href="index.html"; // переадресовываем на главную
+                    location.href = "index.html"; // переадресовываем на главную
                 }
             })
         }
     });
 
+    //Очистка форм
     $formSend.on('reset', function () {
         validate.clearAll($(this))
     });
 
+    //Валидация изменений в инпутах формы
     $('.form__input').on('keyup', function () {
         validate.checkInput($(this))
     });
