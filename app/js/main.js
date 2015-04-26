326b630c5a560dccfcc7fe977e944ba441664176
urla <urla48@yandex.ru>
2015-04-26 20:33:01 +0300

    right img in popups

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,7 @@ $(function () {
             $el: $(selector),
             init: function () {
                 var t = this;
-                this.$el.find('.window-pop__close, .pop-up__overlay').on('click', function () {
+                this.$el.find('.close, .pop-up__overlay').on('click', function () {
                     t.hide();
                 });
             },
@@ -30,25 +30,29 @@ $(function () {
     var galleryPopup = new Popup('.pop-up_gallery');
 
     $('.portfolio__img-hover').on('click', function () {
+        galleryPopup.$el.find('.gallery__img').attr('src', $(this).data('img'));
         galleryPopup.show();
     });
+    var successPopup = new Popup('.pop-up_success');
 
     var validate = {
         checkData: function ($form) {
             this.$form = $form;
             var t = this;
+            this.valid = true;
 
             this.$form.find('.form__input').each(function(){
                 t.checkInput($(this));
             });
 
-            return false;
+            return this.valid;
         },
 
         checkInput: function ($input) {
             if($input.val() == ''){
                 this.showTooltip($input);
                 this.addError($input);
+                this.valid = false;
             } else {
                 this.clearOne($input);
             }
@@ -68,36 +72,29 @@ $(function () {
             this.$form.find('.form__input').each(function(){
                 t.clearOne($(this))
             });
-
-            return false;
         },
 
         clearOne: function ($input) {
-
             $input.closest('.form__group').find('.tooltip').css({'display': 'none'});
             $input.removeClass('error');
-
         }
     };
 
     var $form = $('.form');
 
     $form.on('submit', function (e) {
-        if(!validate.checkData($(this))) {
-            e.preventDefault();
+        e.preventDefault();
+        if(validate.checkData($(this))) {
+            addProjectPopup.hide();
+            successPopup.show();
         }
     });
     $form.on('reset', function () {
-        validate.clear($(this))
+        validate.clearAll($(this))
     });
 
     $('.form__input').on('keyup', function () {
         validate.checkInput($(this))
     });
-
-    $('.window-pop__close, .pop-up__overlay').on('click', function () {
-
-    });
-
 });
 
